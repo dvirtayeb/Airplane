fb395ad9fd93d59fb9f71bc4e9a92db5333e8707
dvirtayeb <44424737+dvirtayeb@users.noreply.github.com>
2020-06-02 19:29:34 +0300

    Add files via upload

---
+++ AirPlane-projectKP/WebServerAirplane.py
@@ -0,0 +1,25 @@
+import subprocess
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/AirPlane-form")
+def airplane():
+    return render_template('AirPlane-form.html')
+
+
+@app.route("/")
+def import_java_file():
+    return subprocess.check_output(
+        ["java", "-classpath", "/Users/dvir tayeb/eclipse-workspace/Airplane/bin/airplane", "Program"])
+
+
+@app.route("/javaFile")
+def import_java_file1():
+    return subprocess.check_output(["java", "-classpath", "/Users/dvir tayeb/eclipse-workspace/UseMVC/bin", "Program"])
+
+
+# reset the app:
+if __name__ == '__main__':
+    app.run(debug=True)
