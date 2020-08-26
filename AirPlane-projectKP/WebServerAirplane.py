50273b7b28f175b417d345c253463d92e5ab4ecb
dvirtayeb <44424737+dvirtayeb@users.noreply.github.com>
2020-08-26 20:50:37 +0300

    Add files via upload

--- AirPlane-projectKP/WebServerAirplane.py
+++ AirPlane-projectKP/WebServerAirplane.py
@@ -6,22 +6,36 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
+def import_java_file(departure_arrival_string, start_date_string, end_date_string,
+                     company_string, state_string, city_string, airport_string, days_string):
+    return subprocess.check_output(
+        ["java", "-classpath", "/Users/dvir tayeb/eclipse-workspace/Airplane/bin", "airplane.Program",
+         "html", departure_arrival_string, start_date_string, end_date_string,
+         company_string, state_string, city_string, airport_string, days_string], universal_newlines=True)
+
+
 @app.route("/", methods=['POST', 'GET'])
 def search():
-    def import_java_file(departure_arrival_string, start_date_string, end_date_string,
-                         company_string, state_string, city_string, airport_string, days_string):
-        return subprocess.check_output(
-            ["java", "-classpath", "/Users/dvir tayeb/eclipse-workspace/Airplane/bin", "airplane.Program",
-             "html", departure_arrival_string, start_date_string, end_date_string,
-             company_string, state_string, city_string, airport_string, days_string], universal_newlines=True)
     if request.method == 'POST':
         departure_arrival = request.form.get("Departure-Arrival")
         company = request.form.get('Company')
+        if company == '':
+            company = "null"
         state = request.form.get('state')
+        if state == '':
+            state = "null"
         city = request.form.get('city')
+        if city == '':
+            city = "null"
         airport = request.form.get('airport')
+        if airport == '':
+            airport = "null"
         start_date = request.form.get('start_date')
+        if start_date == '':
+            start_date = '1-1-1'
         end_date = request.form.get('end_date')
+        if end_date == '':
+            end_date = '1-1-1'
         day = request.form.get('day')
         return render_template('Flight.html',
                                departure_arrival=departure_arrival,
